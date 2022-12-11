d7bcec8032593a086d6473b08dc201e12c5ed574
jakbin <jakbin4747@gmail.com>
2022-12-11 11:05:30 +0530

    json to yaml and yaml to json

--- devtoys/main.py
+++ devtoys/main.py
@@ -16,10 +16,20 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.fsrmatters_button.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.farmatters_page))
         self.convertors_button.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.converters_page))
 
-        self.jsonformate_button.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.json_formate_page))
-        self.jsonformate_button_2.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.json_formate_page))
-
+        # json format
+        self.jsonformate_button.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.json_formate_page)) # for home page
+        self.jsonformate_button_2.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.json_formate_page)) # for formatters page
         self.json_format_button.clicked.connect(self.format_json)
+
+        # json to yaml
+        self.jsontoyaml_button.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.jsontoyaml_page)) # for home page
+        self.jsontoyaml_button_2.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.jsontoyaml_page)) # for converts page
+        self.jsontoyaml_convert_button.clicked.connect(self.json_to_yaml)
+
+        # yaml to json
+        self.yamltojson_button.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.yamltojson_page)) # for home page
+        self.yamltojson_button_2.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.yamltojson_page)) # for converts page
+        self.yamltojson_convert_button.clicked.connect(self.yaml_to_json)
 
     def format_json(self):
         text = self.json_input.toPlainText()
@@ -28,7 +38,13 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.json_output.setText(rdata)
 
     def json_to_yaml(self):
-        text = self.textEdit.toPlainText()
+        text = self.json_input_2.toPlainText()
         dict_data = json.loads(text)
-        data = yaml.dump(dict_data)
-        self.textEdit_2.setText(data)
+        data = yaml.dump(dict_data, indent=4)
+        self.yaml_output_2.setText(data)
+
+    def yaml_to_json(self):
+        text = self.yaml_input_3.toPlainText()
+        dict_data = yaml.safe_load(text)
+        data = json.dumps(dict_data, indent=4)
+        self.json_output_3.setText(data)
